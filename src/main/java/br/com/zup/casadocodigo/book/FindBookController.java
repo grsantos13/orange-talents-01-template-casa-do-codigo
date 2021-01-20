477e336e0520ceac318e0b793cb277271a9303a5
Gustavo Santos <gsantoset@gmail.com>
2021-01-20 10:56:46 -0300

    Reimportado a exceção de recurso não encontrado.

--- src/main/java/br/com/zup/casadocodigo/book/FindBookController.java
+++ src/main/java/br/com/zup/casadocodigo/book/FindBookController.java
@@ -1,6 +1,6 @@
 package br.com.zup.casadocodigo.book;
 
-import br.com.zup.casadocodigo.shared.ResourceNotFoundException;
+import br.com.zup.casadocodigo.shared.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
