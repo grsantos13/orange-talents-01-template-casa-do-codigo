0acae170eecca2be83f5f1f269e64cf92e06743f
Gustavo Santos <gsantoset@gmail.com>
2021-01-20 12:41:35 -0300

    Passado o location via header após cadastro de livro.

--- src/main/java/br/com/zup/casadocodigo/book/NewBookController.java
+++ src/main/java/br/com/zup/casadocodigo/book/NewBookController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/books")
@@ -20,11 +22,17 @@ public class NewBookController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<FullBookResponse> createBook(@RequestBody @Valid NewBookRequest bookRequest){
+    public ResponseEntity<FullBookResponse> createBook(@RequestBody @Valid NewBookRequest bookRequest,
+                                                       UriComponentsBuilder builder){
         Book book = bookRequest.toModel(manager);
         manager.persist(book);
+
+        URI location = builder.path("/books/{id:\\d+}")
+                    .buildAndExpand(book.getId())
+                    .toUri();
+
         FullBookResponse response = book.toFullBookResponse();
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok().location(location).body(response);
     }
 
 }
